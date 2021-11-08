8f837b781270f5835040f8aeade826ff17ae1bc5
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-08 11:47:01 -0600

    created a static method that creates a new Person object that takes in a string to fill in the field of the object.

--- IntroClassesCs/Program.cs
+++ IntroClassesCs/Program.cs
@@ -10,14 +10,22 @@ namespace IntroClassesCs
         {
             Console.WriteLine("Hi {0}, I am {1}", to, Name);
         }
+
+        public static Person Parse(string str)
+        {
+            Person person = new Person();
+            person.Name = str;
+            return person;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Name = "Masen";
+            //Person person = new Person();
+            Person person = Person.Parse("Masen");
+            //person.Name = "Masen";
             person.Introduce("Austin");
         }
     }
